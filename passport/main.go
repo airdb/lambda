465987c6c0695772583780eaebf38fca4163b0a4
Dean <dean@airdb.com>
2018-02-12 19:36:08 +0800

    auot make update

--- passport/main.go
+++ passport/main.go
@@ -9,8 +9,12 @@ import (
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	fmt.Println("Received body: ", request.Body)
 
-	msg := `{"token":"xxxxxxx","status":0}`
-	return events.APIGatewayProxyResponse{Body: msg, StatusCode: 200}, nil
+	msg := `{"token":"416c9006ca431f503160b7e7","status":0}`
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Headers:    map[string]string{"x-airdb-token": "416c9006ca431f503160b7e7"},
+		Body:       msg,
+	}, nil
 }
 
 func main() {
