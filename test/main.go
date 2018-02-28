73b27952a5e8302814d957a11673f2bba6895746
Dean <dean@airdb.com>
2018-02-28 10:52:40 +0800

    update lambda test

---
+++ test/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	fmt.Println("Received body: ", request.Body)
+
+	msg := `{"token":"416c9006ca431f503160b7e7","status":0}`
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Headers:    map[string]string{"x-airdb-token": "416c9006ca431f503160b7e7", "Access-Control-Allow-Origin": "*"},
+		Body:       msg,
+	}, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
